0b27f69480c18cd3d6aaf553b6a4791654f278e2
Mustfa-IT <fuzemustfa@gmail.com>
2025-04-21 00:57:30 +0300

    add new permissions for user management and infractions handling in pb_schema.json; refactor main.go for improved error handling and event binding

--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
-    "log"
-    "os"
+	"log"
 
-    "github.com/pocketbase/pocketbase"
-    "github.com/pocketbase/pocketbase/apis"
-    "github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 )
 
 func main() {
-    app := pocketbase.New()
+	app := pocketbase.New()
 
-    app.OnServe().BindFunc(func(se *core.ServeEvent) error {
-        // serves static files from the provided public dir (if exists)
-        se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 
-        return se.Next()
-    })
-
-    if err := app.Start(); err != nil {
-        log.Fatal(err)
-    }
+	app.OnRecordAfterCreateSuccess("infractions").BindFunc(func(e *core.RecordEvent) error {
+		record, err := app.FindRecordById("users", e.Record.Get("user").(string))
+		if err != nil {
+			log.Default().Println("Error: Record for User Not Found ", record.Id)
+			return err
+		}
+		var s = record.Get("infractions").([]string)
+		s = append(s, e.Record.Get("id").(string))
+		record.Set("infractions", s)
+		err = app.Save(record)
+		if err != nil {
+			log.Default().Println("Error Saving Record for User ", record.Id)
+			return err
+		}
+		return e.Next()
+	})
 }
